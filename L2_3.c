072a42f20f5f8df880b3bcb503b7a1b36471d20d
Himanshu03 <himanshumandal987654@gmail.com>
2025-09-23 19:49:02 +0530

    Delete multiple files

--- L2_3.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int dividend , divisor1 , divisor2;
-    printf("Enter a divident, first divisor, second divisor");
-    scanf("%d %d %d",&dividend,&divisor1,&divisor2);
-    printf("%d",dividend%divisor1==0 && dividend%divisor2==0);
-    return 0;
-}
--- L2_4.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a, b;
-    printf("Enter two intsegers");
-    scanf("%d %d",&a,&b);
-    printf("%d",a==b);
-    return 0;
-}
